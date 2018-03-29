63f1793fa272c16c18bf71da683837aa8b27b494
PixelZer0 <pixel_zero@hotmail.com>
2018-03-29 17:01:01 +0200

    Added some kata's to 'strings.rb' file

--- Basic katas/strings.rb
+++ Basic katas/strings.rb
@@ -9,3 +9,25 @@ print "Hola, Ruby"
 # 3 - Muestra "Hola, Ruby", incluyendo las comillas, y salta de linea
 
 p "Hola, Ruby"
+
+# 4 - Concatena dos cadenas de texto
+
+puts "Hola," + " Ruby"
+
+# 5 - Muestra 4 veces "Hola, Ruby" y salta de linea
+
+puts "Hola, Ruby" * 4
+
+# 6 - Manda un comando al sistema
+
+puts `ls -l`
+
+# 7 - Transforma un entero en una cadena de texto
+
+puts 68.to_s
+
+# 8 - Concatena un bloque de texto
+
+texto = "Hola, Ruby. "
+texto << "Este texto se ha concatenado al final."
+puts texto
