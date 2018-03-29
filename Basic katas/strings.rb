00773d34b2f68213cbfcc083478ae4cb96164ceb
PixelZer0 <pixel_zero@hotmail.com>
2018-03-29 18:20:52 +0200

    Added more kata's to 'strings.rb' file

--- Basic katas/strings.rb
+++ Basic katas/strings.rb
@@ -31,3 +31,19 @@ puts 68.to_s
 texto = "Hola, Ruby. "
 texto << "Este texto se ha concatenado al final."
 puts texto
+
+# 9 - Realiza una pregunta, vacía la salida del programa, espera una entrada de texto 
+# y devuelve una respuesta
+
+puts "¿Qué lenguaje estás utilizando?"
+STDOUT.flush
+lenguaje = gets.chomp
+puts "Estoy utilizando " + lenguaje
+
+# 10 - Obtiene la longitud de una cadena de texto
+
+puts "Hola, Ruby".length
+
+# 11 - Transforma una expresión aritmética y la muestra en una cadena de texto
+
+puts "31 + 41 = #{31+41}"
