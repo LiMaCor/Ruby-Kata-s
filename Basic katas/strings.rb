7f09264093c968d1db6fd3a09103c1d5a647e9e1
PixelZer0 <pixel_zero@hotmail.com>
2018-03-29 10:34:49 +0200

    Added new kata to 'strings.rb'

--- Basic katas/strings.rb
+++ Basic katas/strings.rb
@@ -5,3 +5,11 @@ puts "Hola, Ruby"
 # 2 - Muestra "Hola, Ruby" por pantalla y no salta de linea
 
 print "Hola, Ruby"
+
+# 3 - Muestra un bloque de texto sin utilizar 'puts' o 'print'
+
+=begin
+Esto es un
+bloque de texto
+con 3 lineas.   
+=end
